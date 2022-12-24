8d8a69e8bfe42e4e5633900bb8299a13ef1536db
Ajaipaul Cheema <ajaipaul_cheema@hotmail.com>
2022-12-23 19:15:42 -0800

    push existing project

---
+++ stock-trading-app/src/pages/Home.tsx
@@ -0,0 +1,8 @@
+export function Home() {
+  return (
+    <>
+      <h3>Do you want to buy shares in different companies? Check out the Stocks tab!</h3>
+      <img src="../public/imgs/stockshares.jpg"/>
+    </>
+  )
+}
